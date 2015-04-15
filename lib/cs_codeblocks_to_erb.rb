f3e28aa844a3bf0126df0d06b818188c12f77a7d
Jonathan Frias <jonathan.frias@ascentis.com>
2015-04-15 16:09:07 -0400

    Handle basic case with multiple blocks

--- lib/cs_codeblocks_to_erb.rb
+++ lib/cs_codeblocks_to_erb.rb
@@ -1,14 +1,17 @@
 require "cs_codeblocks_to_erb/version"
 require "active_support/core_ext/string/inflections"
+require "pry"
 
 module CsCodeblocksToErb
   module_function
   def parse(codeblock)
-    "<%= #{process_block_var(codeblock)} %>"
+    get_codeblocks(codeblock).map do |block|
+      "<%= #{process_block_var(block)} %>"
+    end.join("")
   end
 
   def get_codeblocks(input)
-    input.rpartition(/(<%# \S+ %?>)/)
+    input.rpartition(/(<%# \S+ %?>)/).delete_if { |x| x.size == 0 }
   end
 
   def process_block_var(block_var)
--- spec/parse_spec.rb
+++ spec/parse_spec.rb
@@ -1,12 +1,13 @@
 require "spec_helper"
 describe "parsing cs" do
   {
-    "<%# Subject %>"       => "<%= subject %>",
-    "<%# Subject.Other %>" => "<%= subject.other %>",
-    "<%# OtherSubject %>"  => "<%= other_subject %>"
+    "<%# Subject %>"         => "<%= subject %>",
+    "<%# Subject.Other %>"   => "<%= subject.other %>",
+    "<%# OtherSubject %>"    => "<%= other_subject %>",
+    "<%# OtherSubject %>"*2  => "<%= other_subject %>"*2,
   }.each do |cs_codeblock, erb_compatible|
     it "process #{cs_codeblock} to #{erb_compatible}" do
-      expect( CsCodeblocksToErb.parse(cs_codeblock) ).to eq erb_compatible 
+      expect( CsCodeblocksToErb.parse(cs_codeblock) ).to eq erb_compatible
     end
   end
 end
